9bc06c0cb250a1458aaa37843495624d1eece50c
EduardoUT <mcorey316@gmail.com>
2026-03-07 17:00:50 -0600

    chore: Add PurchaseItem entity with JPA and Lombok Annotations and PurchaseItemId as compound key

---
+++ src/main/java/io/github/eduardout/e_commerce/entity/PurchaseItem.java
@@ -0,0 +1,55 @@
+package io.github.eduardout.e_commerce.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity(name = "purchase_item")
+@IdClass(PurchaseItemId.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString(exclude = {"purchase"})
+@Builder(builderMethodName = "aPurchaseItem", setterPrefix = "with")
+public class PurchaseItem {
+    @Getter
+    @Setter
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "purchase_id", nullable = false)
+    private Purchase purchase;
+    @Getter
+    @Setter
+    @Id
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+    @Getter
+    @Setter
+    @Column(nullable = false)
+    private Integer quantity;
+    @Getter
+    @Setter
+    @Column(name = "line_amount", nullable = false)
+    private BigDecimal lineAmount;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof PurchaseItem other)) {
+            return false;
+        }
+        return purchase != null && product != null
+                && Objects.equals(purchase.getId(), other.getPurchase().getId())
+                && Objects.equals(product.getId(), other.getProduct().getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
