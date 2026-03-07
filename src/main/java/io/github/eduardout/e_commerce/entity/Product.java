26a80b88145493e87dafa8333e74fea3576aed87
EduardoUT <mcorey316@gmail.com>
2026-03-07 17:08:29 -0600

    chore: Add Product entity with JPA and Lombok Annotations

---
+++ src/main/java/io/github/eduardout/e_commerce/entity/Product.java
@@ -0,0 +1,86 @@
+package io.github.eduardout.e_commerce.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+public class Product implements Identifiable<Long> {
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Getter
+    @Setter
+    @Column(nullable = false)
+    private String name;
+    @Getter
+    @Setter
+    @Column(nullable = false)
+    private String description;
+    @Getter
+    @Setter
+    @Column(name = "discount_percentage", nullable = false)
+    private BigDecimal discountPercentage;
+    @Getter
+    @Setter
+    @Column(name = "discount_amount", nullable = false)
+    private BigDecimal discountAmount;
+    @Getter
+    @Setter
+    @Column(name = "sell_price", nullable = false)
+    private BigDecimal sellPrice;
+    @Getter
+    @Setter
+    @Column(name = "purchase_price", nullable = false)
+    private BigDecimal purchasePrice;
+    @Getter
+    @Setter
+    @Column(nullable = false)
+    private Integer stock;
+    @Getter
+    @Setter
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_category_id", nullable = false)
+    private ProductCategory productCategory;
+    @Getter
+    @Setter
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Builder(builderMethodName = "aProduct", setterPrefix = "with")
+    private Product(String name, String description, BigDecimal discountPercentage, BigDecimal discountAmount, BigDecimal sellPrice, BigDecimal purchasePrice, Integer stock,
+                    ProductCategory productCategory) {
+        this.name = name;
+        this.description = description;
+        this.discountPercentage = discountPercentage;
+        this.discountAmount = discountAmount;
+        this.sellPrice = sellPrice;
+        this.purchasePrice = purchasePrice;
+        this.stock = stock;
+        this.productCategory = productCategory;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Product other)) {
+            return false;
+        }
+        return id != null && other.getId() != null && id.equals(other.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
