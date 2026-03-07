cb58b312ae6bfd88b37871e05765f37d7ffb908c
EduardoUT <mcorey316@gmail.com>
2026-03-07 16:56:49 -0600

    chore: Add User entity with JPA and Lombok Annotations

---
+++ src/main/java/io/github/eduardout/e_commerce/entity/User.java
@@ -0,0 +1,68 @@
+package io.github.eduardout.e_commerce.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.jspecify.annotations.NullMarked;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+public class User implements UserDetails, Identifiable<Long> {
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Getter
+    @Setter
+    @Column(length = 50, nullable = false, unique = true)
+    private String username;
+    @Getter
+    @Setter@Column(nullable = false)
+    private String password;
+    @Getter
+    @Setter
+    @Column(length = 20, nullable = false)
+    private String role;
+    @Getter
+    @Setter
+    @Column(length = 92, nullable = false, unique = true)
+    private String email;
+
+    @Builder(builderMethodName = "anUser", setterPrefix = "with")
+    private User(String username, String password, String role, String email) {
+        this(null, username, password, role, email);
+    }
+
+    @NullMarked
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
