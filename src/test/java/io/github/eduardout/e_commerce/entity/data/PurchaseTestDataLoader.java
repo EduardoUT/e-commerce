671e6ca5356bb67bcd6b34e927925b58b312be4e
EduardoUT <mcorey316@gmail.com>
2026-03-08 15:38:14 -0600

    test: Add subclass of TestDataLoader to load and save test entites of type Purchase

---
+++ src/test/java/io/github/eduardout/e_commerce/entity/data/PurchaseTestDataLoader.java
@@ -0,0 +1,43 @@
+package io.github.eduardout.e_commerce.entity.data;
+
+import io.github.eduardout.e_commerce.entity.*;
+import io.github.eduardout.e_commerce.repository.PurchaseRepository;
+
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class PurchaseTestDataLoader extends TestDataLoader<Purchase> {
+
+    private final Set<Product> products;
+    private final Customer customer;
+
+    public PurchaseTestDataLoader(PurchaseRepository purchaseRepository, Set<Product> products, Customer customer) {
+        super(purchaseRepository);
+        validateEntities(products);
+        validateEntity(customer);
+        this.customer = customer;
+        this.products = products;
+    }
+
+    @Override
+    protected void setDefaultTestEntities() {
+        Set<Purchase> purchases = products.stream()
+                .map(product -> {
+                    Integer quantity = 5;
+                    BigDecimal calculation = product.getSellPrice().multiply(new BigDecimal(quantity.toString()));
+                    return Purchase.aPurchase()
+                            .withCustomer(customer)
+                            .withPurchaseItemElement(PurchaseItem.aPurchaseItem()
+                                    .withProduct(product)
+                                    .withQuantity(quantity)
+                                    .withLineAmount(calculation)
+                                    .build())
+                            .withSubTotal(calculation)
+                            .withTotal(calculation)
+                            .build();
+                })
+                .collect(Collectors.toSet());
+        addEntities(purchases);
+    }
+}
