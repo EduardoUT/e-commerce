2f7c1db62933a9463e468e72450d36b134a1370a
EduardoUT <mcorey316@gmail.com>
2026-03-09 14:30:20 -0600

    chore: Add record for Purchase response

---
+++ src/main/java/io/github/eduardout/e_commerce/dto/response/PurchaseResponse.java
@@ -0,0 +1,12 @@
+package io.github.eduardout.e_commerce.dto.response;
+
+import io.github.eduardout.e_commerce.entity.PurchaseItem;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public record PurchaseResponse(Long id, CustomerWithUserResponse customerWithUserResponse,
+                               Set<PurchaseItem> purchaseItems, BigDecimal discountAmount, BigDecimal total,
+                               LocalDateTime createdAt) {
+}
