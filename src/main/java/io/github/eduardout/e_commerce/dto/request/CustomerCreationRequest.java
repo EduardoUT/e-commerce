0f90a0b77a18d18fb83c5d24e53decbbbf7d9664
EduardoUT <mcorey316@gmail.com>
2026-03-09 12:22:03 -0600

    chore: Add record for Customer creation with Jakarta Bean Validation

---
+++ src/main/java/io/github/eduardout/e_commerce/dto/request/CustomerCreationRequest.java
@@ -0,0 +1,10 @@
+package io.github.eduardout.e_commerce.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+
+public record CustomerCreationRequest(
+        @NotNull(message = "User is mandatory")
+        UserCreationRequest userCreationRequest,
+        @NotNull(message = "Personal data is mandatory")
+        PersonalDataCreationRequest personalData) {
+}
