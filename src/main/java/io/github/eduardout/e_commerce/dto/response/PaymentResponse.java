8ccf29f0716f41e418838e62a324003f7ae2b5cf
EduardoUT <mcorey316@gmail.com>
2026-03-09 14:23:03 -0600

    chore: Add record for Payment response

---
+++ src/main/java/io/github/eduardout/e_commerce/dto/response/PaymentResponse.java
@@ -0,0 +1,10 @@
+package io.github.eduardout.e_commerce.dto.response;
+
+import io.github.eduardout.e_commerce.util.PaymentStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record PaymentResponse(Long id, CustomerWithUserResponse customerWithUserResponse, PaymentStatus paymentStatus,
+                              String externalId, String gatewayName, BigDecimal amount, LocalDateTime createdAt) {
+}
