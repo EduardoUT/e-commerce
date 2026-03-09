29d34fefeebba22d734ae58f48fdb2d8c9d22d05
EduardoUT <mcorey316@gmail.com>
2026-03-09 13:17:17 -0600

    chore: Change Not blank to not null validation

--- src/main/java/io/github/eduardout/e_commerce/dto/request/PurchaseItemCreationRequest.java
+++ src/main/java/io/github/eduardout/e_commerce/dto/request/PurchaseItemCreationRequest.java
@@ -10,7 +10,7 @@ public record PurchaseItemCreationRequest(
         ProductResponse productResponse,
         @Size(min = 1, message = "Product quantity out of range")
         Integer quantity,
-        @NotBlank(message = "Line amount is mandatory")
+        @NotNull(message = "Line amount is mandatory")
         @Digits(integer = 7, fraction = 2, message = """
                 Maximum limit of integer digits must be less or equal to 7 and decimal are 2. 
                 """)
