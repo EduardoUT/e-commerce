9e100cefa8e8625f53cd5e4b4f908418ae076dbc
EduardoUT <mcorey316@gmail.com>
2026-03-09 13:31:11 -0600

    chore: Change validations on external and internal number parameters

--- src/main/java/io/github/eduardout/e_commerce/dto/request/AddressCreationRequest.java
+++ src/main/java/io/github/eduardout/e_commerce/dto/request/AddressCreationRequest.java
@@ -3,6 +3,7 @@ package io.github.eduardout.e_commerce.dto.request;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Null;
 
 public record AddressCreationRequest(
         @NotBlank(message = "Street is mandatory")
@@ -16,11 +17,12 @@ public record AddressCreationRequest(
         @NotBlank(message = "Zip code is mandatory")
         String zipCode,
         @NotBlank(message = "External number is mandatory")
-        @Min(1)
-        @Max(Integer.MAX_VALUE)
+        @Min(value = 1, message = "External number must not be negative")
+        @Max(value = Integer.MAX_VALUE, message = "External number out of maximum range")
         Integer externalNumber,
-        @Min(1)
-        @Max(Integer.MAX_VALUE)
+        @Null
+        @Min(value = 1, message = "Internal number must not be negative")
+        @Max(value = Integer.MAX_VALUE, message = "Internal number out of maximum range")
         Integer internalNumber
 ) {
 }
