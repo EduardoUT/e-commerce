10e344fdbdde561af98205d26928ea7ec61ddf2e
EduardoUT <mcorey316@gmail.com>
2026-03-09 13:42:26 -0600

    chore: Add record for Address response

---
+++ src/main/java/io/github/eduardout/e_commerce/dto/response/AddressResponse.java
@@ -0,0 +1,5 @@
+package io.github.eduardout.e_commerce.dto.response;
+
+public record AddressResponse(Long id, String street, String neighborhood, String municipality,
+                              String state, String zipCode, Integer externalNumber, Integer internalNumber) {
+}
