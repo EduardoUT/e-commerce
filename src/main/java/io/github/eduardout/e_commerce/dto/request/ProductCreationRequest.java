d75d61e15482f0b087c843baa3f1718f5143e12f
EduardoUT <mcorey316@gmail.com>
2026-03-09 12:58:35 -0600

    chore: Add record for Product creation with Jakarta Bean Validation

---
+++ src/main/java/io/github/eduardout/e_commerce/dto/request/ProductCreationRequest.java
@@ -0,0 +1,26 @@
+package io.github.eduardout.e_commerce.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.math.BigDecimal;
+
+public record ProductCreationRequest(
+        @NotBlank(message = "Name is mandatory")
+        @Size(min = 3, max = 50, message = "Product name is out of range which is minimum 3 and maximum 50 characters")
+        String name,
+        @NotBlank(message = "Description is mandatory")
+        @Size(min = 8, max = 250, message = "Product description is out of range which is minimum 8 and maximum 250 " +
+                "characters")
+        String description,
+        @NotBlank(message = "Sell price is mandatory")
+        BigDecimal sellPrice,
+        @NotBlank(message = "Purchase price is mandatory")
+        BigDecimal purchasePrice,
+        @Size(min = 1, message = "Stock out of range")
+        Integer stock,
+        @NotNull(message = "Must specify a product category")
+        ProductCategoryCreationRequest productCategoryCreationRequest
+) {
+}
