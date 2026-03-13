76fd989f1140c84120d7644142da025ac1cb753e
EduardoUT <mcorey316@gmail.com>
2026-03-13 12:28:22 -0600

    refactor: Change valid default address details

--- src/test/java/io/github/eduardout/e_commerce/entity/data/builder/Addresses.java
+++ src/test/java/io/github/eduardout/e_commerce/entity/data/builder/Addresses.java
@@ -5,10 +5,10 @@ import io.github.eduardout.e_commerce.entity.Address;
 public class Addresses {
     public static Address.AddressBuilder anAddress() {
         return Address.anAddress()
-                .withStreet("Some street")
-                .withNeighborhood("Some neighborhood")
-                .withMunicipality("Some municipality")
-                .withState("Some state")
+                .withStreet("Circuito Interior 1")
+                .withNeighborhood("Miguel Hidalgo")
+                .withMunicipality("CDMX")
+                .withState("CDMX")
                 .withZipCode("54213")
                 .withExternalNumber(50)
                 .withInternalNumber(2);
