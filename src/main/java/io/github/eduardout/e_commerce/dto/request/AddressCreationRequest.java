039ffe99e01879578d2444a97f3984f5b0b4dfdc
EduardoUT <mcorey316@gmail.com>
2026-03-13 12:26:22 -0600

    refactor: Add bean validation annotations

--- src/main/java/io/github/eduardout/e_commerce/dto/request/AddressCreationRequest.java
+++ src/main/java/io/github/eduardout/e_commerce/dto/request/AddressCreationRequest.java
@@ -4,25 +4,25 @@ import jakarta.validation.constraints.*;
 
 public record AddressCreationRequest(
         @NotBlank(message = "Street is mandatory")
-        @Size(max = 30, message = "Out of range, max characters for street are 30")
+        @Size(max = 70, message = "Out of range, max characters for street are 70")
         String street,
         @NotBlank(message = "Neighborhood is mandatory")
-        @Size(max = 30, message = "Out of range, max characters for neighborhood are 30")
+        @Size(max = 70, message = "Out of range, max characters for neighborhood are 70")
         String neighborhood,
         @NotBlank(message = "Municipality is mandatory")
-        @Size(max = 30, message = "Out of range, max characters for municipality are 30")
+        @Size(max = 50, message = "Out of range, max characters for municipality are 50")
         String municipality,
         @NotBlank(message = "State is mandatory")
-        @Size(max = 30, message = "Out of range, max characters for state are 30")
+        @Size(max = 50, message = "Out of range, max characters for state are 50")
         String state,
-        @NotBlank(message = "Zip code is mandatory")
-        @Size(max = 15, message = "Out of range, max characters for zipCode are 15")
+        @NotNull(message = "Zip code is mandatory")
+        @Pattern(regexp = "^\\d{5}$", message = "The zip code must be of 5 numeric digits, and must not contain spaces")
         String zipCode,
         @NotNull(message = "External number should not be null")
         @Min(value = 1, message = "External number must not be negative")
-        @Max(value = Integer.MAX_VALUE, message = "External number out of maximum range")
+        @Max(value = Integer.MAX_VALUE)
         Integer externalNumber,
-        @PositiveOrZero(message = "Internal number should not be negative, if omitted type null")
+        @Positive(message = "Internal number should not be negative, if omitted type null")
         Integer internalNumber
 ) {
     public AddressCreationRequest {
