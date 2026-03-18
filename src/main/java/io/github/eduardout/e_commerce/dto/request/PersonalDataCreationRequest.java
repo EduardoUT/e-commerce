b652d0e979d7a0682ab01fa1e9a7eaad9a7ee795
EduardoUT <mcorey316@gmail.com>
2026-03-18 17:28:46 -0600

    chore: Change validations and message templates and adding custom bean validation

--- src/main/java/io/github/eduardout/e_commerce/dto/request/PersonalDataCreationRequest.java
+++ src/main/java/io/github/eduardout/e_commerce/dto/request/PersonalDataCreationRequest.java
@@ -1,25 +1,48 @@
 package io.github.eduardout.e_commerce.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
+import io.github.eduardout.e_commerce.dto.constraint.annotation.CheckOnlyLetters;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 public record PersonalDataCreationRequest(
-        @NotBlank
-        @Size(min = 2, max = 50, message = "First name is out of range")
+        @NotNull(message = "First name is mandatory")
+        @CheckOnlyLetters
+        @Size(min = 2, max = 30, message = "First name is out of range")
         String firstName,
-        @NotBlank
-        @Size(min = 2, max = 50, message = "Maternal surname is out of range")
+        @NotNull(message = "Maternal surname is mandatory")
+        @CheckOnlyLetters
+        @Size(min = 2, max = 30, message = "Maternal surname is out of range")
         String maternalSurname,
-        @NotBlank
-        @Size(min = 2, max = 50, message = "Paternal surname is Out of range")
+        @NotNull(message = "Paternal surname is mandatory")
+        @CheckOnlyLetters
+        @Size(min = 2, max = 30, message = "Paternal surname is out of range")
         String paternalSurname,
-        @NotNull(message = "Birth date must not be null")
+        @NotNull(message = "Birth date is mandatory")
         LocalDate birthDate,
-        @NotBlank
-        @Size(min = 8, max = 15, message = "Out of range, phone number must be between 8 and 15 digits")
+        @NotNull(message = "Phone number is mandatory")
+        @Pattern(regexp = "^(?:\\+?\\d{1,3}(?:-?\\d{3,4})?)?-?\\d+(?:-?\\d{2}){4}$", message = """
+                Phone number is invalid:
+                - It should not start or end whit white spaces
+                - Use only single hyphens as separators
+                - White spaces aren't allowed
+                - Plus symbol is optional
+                - Dialing code (area code) is mandatory
+                """)
+        @Size(min = 10, max = 24, message = "Out of range, phone number must be between 10 and 24 digits including " +
+                "area code")
         String phoneNumber
 ) {
+    public PersonalDataCreationRequest {
+        if (birthDate != null) {
+            Integer currentYear = LocalDate.now().getYear();
+            Integer birthYear = birthDate.getYear();
+            int age = currentYear - birthYear;
+            if (age < 18) {
+                throw new IllegalArgumentException("You are under age");
+            }
+        }
+    }
 }
