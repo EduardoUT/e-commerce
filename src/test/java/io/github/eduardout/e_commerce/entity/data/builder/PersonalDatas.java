34df6555cecb80bafc6f0f0b75a0b1a170f2a8b2
EduardoUT <mcorey316@gmail.com>
2026-03-17 18:36:00 -0600

    test: Change order of builder according to entity atributes

--- src/test/java/io/github/eduardout/e_commerce/entity/data/builder/PersonalDatas.java
+++ src/test/java/io/github/eduardout/e_commerce/entity/data/builder/PersonalDatas.java
@@ -8,8 +8,8 @@ public class PersonalDatas {
     public static PersonalData.PersonalDataBuilder aPersonalData() {
         return PersonalData.aPersonalData()
                 .withFirstName("John")
-                .withPaternalSurname("Doe")
                 .withMaternalSurname("Dow")
+                .withPaternalSurname("Doe")
                 .withBirthDate(LocalDate.of(1982, 3, 22))
                 .withPhoneNumber("55-45465461");
     }
