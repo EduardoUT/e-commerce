144aa9440b5972270b2ae5c8b498498a494a4a1f
EduardoUT <mcorey316@gmail.com>
2026-03-28 14:50:17 -0600

    docs: Add documentation for Identifiable and method interface

--- src/main/java/io/github/eduardout/e_commerce/entity/Identifiable.java
+++ src/main/java/io/github/eduardout/e_commerce/entity/Identifiable.java
@@ -1,5 +1,14 @@
 package io.github.eduardout.e_commerce.entity;
 
-public interface Identifiable <T> {
+/**
+ * Specifies an object that represents an entity and must provide a qualified inmutable and unique identifier.
+ *
+ * @param <T> The type of the identifier.
+ */
+@FunctionalInterface
+public interface Identifiable<T> {
+    /**
+     * @return An unique and inmutable identifier.
+     */
     T getId();
 }
