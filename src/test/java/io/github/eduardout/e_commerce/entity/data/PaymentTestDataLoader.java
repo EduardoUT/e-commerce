cacfef2c2707b5dd9e4fc39e15781d8b6711316e
EduardoUT <mcorey316@gmail.com>
2026-03-28 16:45:43 -0600

    test: Add PaymentStatus to default test payments

--- src/test/java/io/github/eduardout/e_commerce/entity/data/PaymentTestDataLoader.java
+++ src/test/java/io/github/eduardout/e_commerce/entity/data/PaymentTestDataLoader.java
@@ -4,6 +4,7 @@ import io.github.eduardout.e_commerce.entity.Customer;
 import io.github.eduardout.e_commerce.entity.Payment;
 import io.github.eduardout.e_commerce.entity.data.builder.Payments;
 import io.github.eduardout.e_commerce.repository.PaymentRepository;
+import io.github.eduardout.e_commerce.util.PaymentStatus;
 
 import java.math.BigDecimal;
 import java.util.*;
@@ -28,12 +29,14 @@ public class PaymentTestDataLoader extends TestDataLoader<Payment> {
     protected void setDefaultTestEntities() {
         addEntity(Payments.aPayment()
                 .withCustomer(customer)
+                .withPaymentStatus(PaymentStatus.PENDING)
                 .withExternalId("st_dnjaknjkas4654654")
                 .withGatewayName("stripe")
                 .withAmount(new BigDecimal("600.00"))
                 .build());
         addEntity(Payments.aPayment()
                 .withCustomer(customer)
+                .withPaymentStatus(PaymentStatus.PENDING)
                 .withExternalId("st_ashkjhkasjk445456989")
                 .withGatewayName("stripe")
                 .withAmount(new BigDecimal("350.00"))
