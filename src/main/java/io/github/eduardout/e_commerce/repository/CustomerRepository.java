45879befe3ab72d7aac267ad03f319c67b447ff2
EduardoUT <mcorey316@gmail.com>
2026-03-28 17:03:19 -0600

    refactor: Add JPQL query and EntityGraph to retrieve a Customer with User details

--- src/main/java/io/github/eduardout/e_commerce/repository/CustomerRepository.java
+++ src/main/java/io/github/eduardout/e_commerce/repository/CustomerRepository.java
@@ -5,6 +5,7 @@ import org.jspecify.annotations.NullMarked;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,21 +14,16 @@ import java.util.Optional;
 @Repository
 @NullMarked
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    @Override
-    @EntityGraph(attributePaths = {"user", "personalData", "address"})
-    Optional<Customer> findById(Long id);
+    @Query(value = """
+            SELECT c FROM Customer c
+            WHERE c.id = :id
+            """)
+    @EntityGraph(attributePaths = {"user"})
+    Optional<Customer> findByIdWithUser(@Param("id") Long id);
 
-    @EntityGraph(attributePaths = {"user", "personalData", "address"})
     Optional<Customer> findByUserUsername(String username);
 
     @Override
     @EntityGraph(attributePaths = {"user", "personalData", "address"})
     List<Customer> findAll();
-
-    @Query(value = """
-             SELECT DISTINCT c FROM Customer c
-             LEFT JOIN FETCH c.shoppingCart sc
-             LEFT JOIN FETCH sc.cartItems
-            """)
-    List<Customer> findAllWithShoppingCart();
 }
