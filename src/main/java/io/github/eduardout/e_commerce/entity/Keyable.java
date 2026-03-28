3aeb3fed9439b6906d62f08452f28d91a6a31f4d
EduardoUT <mcorey316@gmail.com>
2026-03-28 14:25:02 -0600

    chore: Add interface to map composite key classes or records

---
+++ src/main/java/io/github/eduardout/e_commerce/entity/Keyable.java
@@ -0,0 +1,14 @@
+package io.github.eduardout.e_commerce.entity;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * Maps to a composite primary key complying with the serialization rule.
+ * </p>
+ * <p>
+ * Implementations must accomplish with the specifications in {@link jakarta.persistence.IdClass}
+ * </p>
+ */
+public interface Keyable extends Serializable {
+}
