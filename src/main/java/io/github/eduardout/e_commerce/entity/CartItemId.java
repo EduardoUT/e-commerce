7f67b33ab1775adb7d50ad79cde5663b0e1e19d6
EduardoUT <mcorey316@gmail.com>
2026-03-28 14:36:00 -0600

    refactor: Change to record and implementing Keyable interface

--- src/main/java/io/github/eduardout/e_commerce/entity/CartItemId.java
+++ src/main/java/io/github/eduardout/e_commerce/entity/CartItemId.java
@@ -1,35 +1,4 @@
 package io.github.eduardout.e_commerce.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.io.Serializable;
-import java.util.Objects;
-
-@NoArgsConstructor
-@AllArgsConstructor
-public class CartItemId implements Serializable {
-    @Getter
-    private Long shoppingCart;
-    @Getter
-    private Long product;
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-
-        if (!(obj instanceof CartItemId other)) {
-            return false;
-        }
-        return Objects.equals(getShoppingCart(), other.getShoppingCart())
-                && Objects.equals(getProduct(), other.getProduct());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getShoppingCart(), getProduct());
-    }
+public record CartItemId (Long shoppingCart, Long product) implements Keyable {
 }
