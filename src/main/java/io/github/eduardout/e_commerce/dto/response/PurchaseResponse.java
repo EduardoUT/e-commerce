0ebe4c1b47d086af34066ca634f4c5f99839a46b
EduardoUT <mcorey316@gmail.com>
2026-03-31 17:44:26 -0600

    refactor: Change sub-interface Collection Set to List

--- src/main/java/io/github/eduardout/e_commerce/dto/response/PurchaseResponse.java
+++ src/main/java/io/github/eduardout/e_commerce/dto/response/PurchaseResponse.java
@@ -4,11 +4,11 @@ import io.github.eduardout.e_commerce.entity.PurchaseItem;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Set;
+import java.util.List;
 
 public record PurchaseResponse(Long id,
                                CustomerWithUserResponse customerWithUserResponse,
-                               Set<PurchaseItem> purchaseItems,
+                               List<PurchaseItem> purchaseItems,
                                BigDecimal discountAmount,
                                BigDecimal total,
                                LocalDateTime createdAt) {
